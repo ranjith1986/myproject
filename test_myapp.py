5390d66f5e9e13c09df37d89c0b208862086fe14
Ranjith Mohan <86.ranjith@gmail.com>
2022-03-31 20:29:01 +0530

    new code

---
+++ test_myapp.py
@@ -0,0 +1,8 @@
+# test_hello.py
+from myapp import app
+
+def test_hello():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Hey, this is my test app'
