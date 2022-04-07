bd406e8e47b50c59f6707535875934a7b4d1d0cb
ranjith1986 <102591467+ranjith1986@users.noreply.github.com>
2022-04-07 12:29:47 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-	return 'Hey, this is my test app'
+    return 'Hey, this is my test app'
 
 # main driver function
 if __name__ == '__main__':
 
 	# run() method of Flask class runs the application
 	# on the local development server.
-	app.run()
+    app.run()
